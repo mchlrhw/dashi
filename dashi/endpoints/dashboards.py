557bf570e645e8903b8beb52cadfa0cb34d20717
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-01-15 21:17:55 +0000

    Factor out JSON object creation

--- dashi/endpoints/dashboards.py
+++ dashi/endpoints/dashboards.py
@@ -6,20 +6,21 @@ from tornado_sqlalchemy import SessionMixin, as_future
 from ..model import Dashboard
 
 
+def dashboard_to_json(row):
+    json_obj = {
+        "id": row.id,
+        "createdAt": row.createdAt.isoformat(),
+        "updatedAt": row.updatedAt.isoformat(),
+        "title": row.title,
+    }
+    return json_obj
+
+
 class DashboardsHandler(SessionMixin, RequestHandler):
     async def get(self):
         with self.make_session() as session:
             dashboards = await as_future(session.query(Dashboard).all)
-
-            serializable = []
-            for row in dashboards:
-                dashboard = {
-                    "id": row.id,
-                    "createdAt": row.createdAt.isoformat(),
-                    "updatedAt": row.updatedAt.isoformat(),
-                    "title": row.title,
-                }
-                serializable.append(dashboard)
+            serializable = [dashboard_to_json(row) for row in dashboards]
 
         payload = json.dumps(serializable)
         self.write(payload)
