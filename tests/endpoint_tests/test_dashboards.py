ce60cbc4515a1cea4bd3c6b0f81426d638f1aacb
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-01-15 19:50:04 +0000

    Recreate smoke_tests.sh as test in test suite

--- tests/endpoint_tests/test_dashboards.py
+++ tests/endpoint_tests/test_dashboards.py
@@ -1,7 +1,26 @@
+import json
+from urllib.parse import urljoin
+
 import pytest
 
 
 @pytest.mark.gen_test
 async def test_dashboards(http_client, base_url):
-    resp = await http_client.fetch(base_url)
+    dashboards_url = urljoin(base_url, "/dashboards")
+
+    resp = await http_client.fetch(dashboards_url)
     assert resp.code == 200
+
+    payload = json.loads(resp.body)
+
+    assert "id" in payload
+    assert payload["id"] > 0
+
+    assert "createdAt" in payload
+    assert payload["createdAt"]
+
+    assert "updatedAt" in payload
+    assert payload["updatedAt"]
+
+    assert "title" in payload
+    assert payload["title"]
